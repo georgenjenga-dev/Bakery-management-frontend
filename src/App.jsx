c100adf293ea2f17b0d0535bf48bf3b9e04e0c92
joshua <joshua.mbilli@student.moringaschool.com>
2026-07-30 08:05:07 +0300

    Configure application routes for admin access

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,28 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import ProtectedRoute from "./components/ProtectedAdminRoute";
+import AdminEntry from "./components/AdminEntry";
 
 import Home from "./pages/Home";
 import Products from "./pages/Products";
 import Cart from "./pages/Cart";
 import Checkout from "./pages/Checkout";
 import Orders from "./pages/Orders";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
 import Contact from "./pages/Contact";
+
+import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 
 function App() {
+  const location = useLocation();
+
+  const isAdminPage = location.pathname.startsWith("/admin");
   
   return (
     <>
-      <Navbar />
+      {!isAdminPage && <Navbar />}
 
       <Routes>
         <Route path="/" element={<Home />} />
@@ -25,13 +30,22 @@ function App() {
         <Route path="/cart" element={<Cart />} />
         <Route path="/checkout" element={<Checkout />} />
         <Route path="/orders" element={<Orders />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
 
-      <Footer />
+        <Route path="/admin" element={<AdminEntry />} />
+        <Route path="/admin/login" element={<Login />} />
+
+  <Route
+    path="/admin/dashboard"
+    element={
+      <ProtectedRoute>
+        <Dashboard />
+      </ProtectedRoute>
+    }
+  />
+</Routes>
+ 
+      {!isAdminPage &&<Footer />}
     </>
   );
 }
