90f4eabdd6396c542d33da5c8eba9ab98bd78530
joshua <joshua.mbilli@student.moringaschool.com>
2026-07-30 08:06:40 +0300

    Implement admin login interface

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,11 @@
+import React from 'react';
 import { Link } from "react-router-dom";
+import { useAuth } from '../contexts/AuthContext';
 import "./Navbar.css";
 
 function Navbar() {
+  const { admin, logout } = useAuth();
+  
   return (
     <header className="navbar">
       <div className="logo">
@@ -14,9 +18,6 @@ function Navbar() {
         <Link to="/cart">Cart</Link>
         <Link to="/orders">Orders</Link>
         <Link to="/contact">Contact</Link>
-        <Link to="/login" className="login-btn">
-          Login
-        </Link>
       </nav>
     </header>
   );
